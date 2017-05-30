69552bf30240089afb0c5659be16b9d027dab122
leo-guggenberger <info@leo-guggenberger.at>
2017-05-30 15:18:57 +0200

    Create __openerp__.py

---
+++ eai_server/__openerp__.py
@@ -0,0 +1,27 @@
+# __openerp__.py
+# -*- coding: utf-8 -*-
+{
+    'name': "Enterprise Application Integration Server",
+    'version': '1.00',
+    'description': """
+Enterprise Application Integration Server
+=============
+        
+Enterprise Application Integration Server with following features:
+-------------------------------------
+* ...
+* Language support: English, German
+* Odoo version support: v8
+        """,
+    'author': 'Leo',
+    'website': 'http://www.leo-guggenberger.at',
+    'category': 'Generic Modules/Others',
+    'summary': 'Enterprise Application Integration Server for Odoo',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'js': [],
+    'installable': True,
+    'auto_install': False,
+}
