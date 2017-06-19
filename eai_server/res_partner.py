e46e5a5bd340c1bc1fadbae1f29742447d9c65b2
leo-guggenberger <info@leo-guggenberger.at>
2017-06-19 15:28:35 +0200

    Update res_partner.py

--- eai_server/res_partner.py
+++ eai_server/res_partner.py
@@ -1,5 +1,6 @@
+# res_partner.py
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo import api, fields, models
 
 # Table inherit partner
